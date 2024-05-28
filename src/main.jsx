a6372fdab643d2018f6855d4ced67891a176205f
Jaermis <jermynejosh.kaquilala@cit.edu>
2024-05-28 21:40:12 +0800

    Added Navbar

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,9 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
-import "@fontsource/outfit"
-import "@fontsource/roboto"
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App.jsx';
+import './index.css';
+import "@fontsource/outfit";
+import "@fontsource/roboto";
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
