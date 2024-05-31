3ace8f7250f7380c74d1e6df7d6dba68b7994331
Jaermis <jermynejosh.kaquilala@cit.edu>
2024-05-31 11:12:15 +0800

    added about section

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import styles from './App.module.css'
 import { Navbar } from './components/Navbar/Navbar';
 import { Hero } from './components/Hero/Hero';
+import { About } from './components/About/About';
 
 
 function App() {
@@ -8,6 +9,7 @@ function App() {
   return <div className={styles.App}>
     <Navbar/>
     <Hero/>
+    <About />
   </div>;
 }
 
---
+++ src/components/About/About.jsx
@@ -0,0 +1,45 @@
+import React from "react";
+import { getImageUrl } from "../../utils";
+import styles from "./About.module.css"
+
+export const About = () => {
+  return (
+    <section className={styles.container} id="about">
+      <h2 className={styles.title}>About</h2>
+      <div className={styles.content}>
+        <img className={styles.aboutImg} src={getImageUrl("about/aboutImage.png")} alt="me sitting" />
+        <ul className={styles.aboutItems}>
+          <li className={styles.aboutItem}>
+            <img className={styles.aboutImage} src={getImageUrl("about/cursorIcon.png")} alt="Cursor icon" />
+            <div className={styles.aboutItemText}>
+              <h3>Frontend Developer</h3>
+              <p>
+                I'm a frontend developer with experience in building responsive
+                sites in Angular and React.
+              </p>
+            </div>
+          </li>
+          <li className={styles.aboutItem}>
+            <img className={styles.aboutImage} src={getImageUrl("about/backendIcon.png")} alt="Cursor icon" />
+            <div className={styles.aboutItemText}>
+              <h3>Backend Developer</h3>
+              <p>
+                I have a little experience with backend using Access and Firebase.
+              </p>
+            </div>
+          </li>
+          <li className={styles.aboutItem}>
+            <img className={styles.aboutImage} src={getImageUrl("about/uiIcon.png")} alt="Cursor icon" />
+            <div className={styles.aboutItemText}>
+              <h3>UI/UX Designer</h3>
+              <p>
+                I have designed a number of UI designs ranging from mobile
+                applications to web applications using Figma.
+              </p>
+            </div>
+          </li>
+        </ul>
+      </div>
+    </section>
+  );
+};
