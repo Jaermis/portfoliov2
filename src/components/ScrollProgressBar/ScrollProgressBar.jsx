6342adc93904733732fc4a11773d2b9c0c35ba8b
Jaermis <jermynejosh.kaquilala@cit.edu>
2024-05-31 20:53:23 +0800

    added custom scrollbar

--- src/components/ScrollProgressBar/ScrollProgressBar.jsx
+++ src/components/ScrollProgressBar/ScrollProgressBar.jsx
@@ -1,26 +1,31 @@
-import React, {useEffect, useState} from 'react';
-import styles from './ScrollProgressBar.module.css';
+import { useEffect, useState } from "react";
+import styles from "./ScrollProgressBar.module.css";
 
 export const ScrollProgressBar = () => {
-    const [scrollWidth, setScrollWidth] = useState(0);
+  const [scrollWidth, setScrollWidth] = useState(0);
 
-    useEffect(() => {
-        const handleScroll = () => {
-            const scrollTop =  document.documentElement.scrollTop;
-            const scrollHeight = document.documentElement.scrollHeight - document.documentElement.clientHeight;
-            const progress = (scrollTop / scrollHeight) * 100;
-            setScrollWidth(progress);
-        };
-    window.addEventListener('scroll', handleScroll);
+  useEffect(() => {
+    const handleScroll = () => {
+      const scrollTop = document.documentElement.scrollTop;
+      const scrollHeight =
+        document.documentElement.scrollHeight -
+        document.documentElement.clientHeight;
+      const progress = (scrollTop / scrollHeight) * 100;
+      setScrollWidth(progress);
+    };
+    window.addEventListener("scroll", handleScroll);
 
     return () => {
-        window.removeEventListener('scroll', handleScroll);
+      window.removeEventListener("scroll", handleScroll);
     };
-}, []);
+  }, []);
 
   return (
     <div className={styles.scrollContainer}>
-        <div className={styles.scrollProgress} style={{width: `${scrollWidth}%`}}></div>
+      <div
+        className={styles.scrollProgress}
+        style={{ width: `${scrollWidth}%` }}
+      ></div>
     </div>
-  )
-}
+  );
+};
